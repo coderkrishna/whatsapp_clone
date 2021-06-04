f6ee83216e67f7d4f7f84f193b827d2ebca55a21
krishna <krishnajupally2000@gmail.com>
2021-06-04 14:42:46 +0530

    realtime chat added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
-import React from 'react'
+import {React,useEffect,useState} from 'react'
 import "./App.css";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Chat from "./components/Chat/Chat";
+import Pusher from 'pusher-js';
+import axios from "./axios";
+
 
 function App() {
+
+  const [messages,setMessages] = useState([]); 
+  
+  useEffect(() => {
+     axios.get('api/v1/messages/sync')
+     .then( response => {
+       setMessages(response.data);     
+     })     
+  },[])
+
+  useEffect(() => {
+    var pusher = new Pusher('8698d31322d7fd9328c1', {
+      cluster: 'ap2'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted',(data) => {
+      /*alert(JSON.stringify(data));*/
+
+
+      setMessages([...messages,data]); 
+    });
+    return () => {
+        channel.unbind_all();
+        channel.unsubscribe();
+    }; 
+  },[messages]);
+
+  console.log(messages)
   return (
     <div className = "app">
       <div className = "app_body">
         <Sidebar />
-        <Chat />
+        <Chat messages = {messages}/>
       </div>      
     </div>
   );
--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React,{ useState } from 'react'
 import "./chat.css"
 import { Avatar,IconButton } from "@material-ui/core";
 import { MicOutlined, InsertEmoticon, SearchOutlined , AttachFile , MoreVert } from "@material-ui/icons"
+import axios from "../../axios";
 
-const chat = () => {
+const Chat = ({messages}) => {
+    
+  const [input,setInput] = useState("");
+
+  const sendMessage = async (e) => {
+         e.preventDefault();
+         await axios.post('api/v1/messages/new',{
+            message : input,
+            name : "krishna",
+            timestamp : "2:00:00 GMT",
+            received : true
+         });
+
+         setInput("");
+  };
+      
     return (
         <div className = "chat">
            <div className = "chat_header">
@@ -26,30 +42,17 @@ const chat = () => {
                </div>
            </div>
            <div className = "chat_body">
-             <p className = "chat_message">
-             <span className="chat_name">Krishna</span>
-             This is the message
-             <span className = "chat_timestamp">
-             {new Date().toUTCString()}
-             </span>
-             </p>
-
-             <p className = "chat_message chat_reciever">
-             <span className="chat_name">Krishna</span>
-             This is the message
-             <span className = "chat_timestamp">
-             {new Date().toUTCString()}
-             </span>
-             </p>
-
-
-             <p className = "chat_message">
-             <span className="chat_name">Krishna</span>
-             This is the message
-             <span className = "chat_timestamp">
-             {new Date().toUTCString()}
-             </span>
-             </p>
+            { 
+              messages.map((message) => 
+              <p className = {`chat_message ${ message.received && "chat_receiver"}`}>
+              <span className="chat_name">{message.name}</span>
+              {message.message}
+              <span className = "chat_timestamp">
+              {message.timestamp}
+              </span>
+              </p>   
+            )
+          }
 
            </div>            
 
@@ -58,8 +61,8 @@ const chat = () => {
              <InsertEmoticon />
              </IconButton>
              <form>
-               <input  placeholder = "Type a message" type = "text" />
-               <button type="submit">Send a Message</button>
+               <input value = {input} onChange ={(e) => setInput(e.target.value)} placeholder = "Type a message" type = "text"/>
+               <button type="submit" onClick = {sendMessage} >Send a Message</button>
              </form>
              <IconButton>
              <MicOutlined />
@@ -69,4 +72,4 @@ const chat = () => {
     )
 }
 
-export default chat
+export default Chat
